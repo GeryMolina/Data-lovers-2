394ddf806b0fd1434cea0d64ba0e12ad1bac34c8
GeryMolina <g.molina.ojeda@gmail.com>
2019-09-30 08:20:06 -0300

    se aplica fetch y se muestran los datos de pokemon en la pantalla

--- src/main.js
+++ src/main.js
@@ -1,1 +1,31 @@
 /* Manejo del DOM */
+
+const button = document.getElementById('button');
+button.addEventListener('click', () => {
+        fetch('./data/pokemon/pokemon.json')
+        .then(res => res.json())
+        .then(data => {
+        const listOfPokemon= data.pokemon;
+        const list= document.getElementById('listUl');
+            list.innerHTML= 
+                listOfPokemon.map(x => {
+                    return `
+                    <li class="liItem">
+                        <img class="imgRaM" src="${x.img}">
+                        <div class="charData">
+                            Numero: #${x.num}
+                            Nombre: ${x.name}
+                            Tipo: ${x.type} 
+                            Debilidad: ${x.weaknesses}
+                        </div>    
+                    </li>
+                        `; 
+                }) .join('');  
+            })
+        .catch(error => error)
+    
+    
+});
+
+
+
